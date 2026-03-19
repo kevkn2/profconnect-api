4dc9932dcaf66eb0c92816c8575002510534a140
kevkn2 <jimmyjuan3@gmail.com>
2026-03-19 13:06:15 +0700

    migration: create professors table

---
+++ internal/database/migrations/000003_create_professors_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE professors (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    university VARCHAR(255) NOT NULL,
+    department VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
